d7ba370fdbb63bc30019e174230cd40a48abb464
rtgodwin <42013175+rtgodwin@users.noreply.github.com>
2024-05-25 14:41:59 -0500

    Update hidden-boot.R

--- R/hidden-boot.R
+++ R/hidden-boot.R
@@ -16,13 +16,21 @@ hidden.boot <- function(model, data, maxpred, bootsize = 1000, bootseed = 1) {
   boots <- matrix(, bootsize, maxpred + 1)
   
   if (model$dist == "Poisson") {
-    for(i in 1:bootsize) {
-      data[[all.vars(formula)[1]]] <- roipp(b, g, X, Z)
-      modboot <- oneinfl(formula = formula, data = data, dist = "Poisson")
-      boots[i, ] <- hidden.values(model = modboot, data = data, maxpred = maxpred)$counterfactual.values
+    if(class(model) == "truncmodel"){
+      for(i in 1:bootsize) {
+        data[[all.vars(formula)[1]]] <- rpp(b, X)
+        modboot <- truncreg(formula = formula, data = data, dist = "Poisson")
+        boots[i, ] <- hidden.values(model = modboot, data = data, maxpred = maxpred)$counterfactual.values
+      }
+    }
+    if(class(model) == "oneinflmodel"){
+      for(i in 1:bootsize) {
+        data[[all.vars(formula)[1]]] <- roipp(b, g, X, Z)
+        modboot <- oneinfl(formula = formula, data = data, dist = "Poisson")
+        boots[i, ] <- hidden.values(model = modboot, data = data, maxpred = maxpred)$counterfactual.values
+      }
     }
   }
-  
   q$se <- sqrt((colSums((boots - colMeans(boots)) ^ 2)) / (bootsize - 1))
   bootsrt <- apply(boots, 2, sort)
   q$lower95 <- bootsrt[bootsize*.025, ]
